f206907d8d918e433df8533ae48da7393ce25a5a
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-06-01 08:58:18 +1200

    Added Swicth

--- Shopping Cart/Shopping Cart/Shopping Cart.cpp
+++ Shopping Cart/Shopping Cart/Shopping Cart.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
-    
+    string usersPick;
+    cout << "Enter the product name (or 'exit' to finish)";
+	cin >> usersPick;
+
+	switch (usersPick)
+	{
+	case 'exit':
+	default:
+		break;
+	}
 }
