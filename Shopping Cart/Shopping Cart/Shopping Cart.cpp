bc9e963fe9c4c184414f4fdd9eb78a79222345bb
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-06-01 09:30:24 +1200

    Finished off all functions
    Completed the base layout of the code. Still needs to be updated tho for error checking

--- Shopping Cart/Shopping Cart/Shopping Cart.cpp
+++ Shopping Cart/Shopping Cart/Shopping Cart.cpp
@@ -2,16 +2,10 @@
 #include <vector>
 using namespace std;
 
-struct Product
-{
-    string name;
-    string brand;
-    int price;
-};
-
 vector<string> name;
 vector<string> brand;
 vector<int> price;
+int i = 0;
 
 void productName(string usersPick) {
     name.push_back(usersPick);
@@ -19,9 +13,19 @@ void productName(string usersPick) {
 }
 
 void productBrand(string usersPick) {
-    cout << "Enter the product brand:\n";
+    cout << "Enter the product brand: ";
     cin >> usersPick;
     brand.push_back(usersPick);
+    productPrice(usersPick);
+}
+
+void productPrice(string usersPick) {
+    cout << "Enter the product price: ";
+    cin >> usersPick;
+    brand.push_back(usersPick);
+    cout << "Added" << name[i] << " to the shopping cart.\n\n";
+    i++;
+    return;
 }
 
 int main()
@@ -29,7 +33,7 @@ int main()
     string usersPick;
     while (usersPick != "exit")
     {
-        cout << "Enter the product name (or 'exit' to finish) ";
+        cout << "Enter the product name (or 'exit' to finish): ";
         cin >> usersPick;
         if (usersPick == "exit") {
             exit(0);
