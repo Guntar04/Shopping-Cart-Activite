262273ef6206f1f141e25ad1ce359fb9f5e5fcc4
Guntar <131307803+Guntar04@users.noreply.github.com>
2023-06-01 09:25:05 +1200

    Added New Functions
    Added functions for both name and brand for the product

--- Shopping Cart/Shopping Cart/Shopping Cart.cpp
+++ Shopping Cart/Shopping Cart/Shopping Cart.cpp
@@ -9,10 +9,19 @@ struct Product
     int price;
 };
 
-vector<Product> items;
+vector<string> name;
+vector<string> brand;
+vector<int> price;
 
 void productName(string usersPick) {
+    name.push_back(usersPick);
+    productBrand(usersPick);
+}
 
+void productBrand(string usersPick) {
+    cout << "Enter the product brand:\n";
+    cin >> usersPick;
+    brand.push_back(usersPick);
 }
 
 int main()
